65ccf2406cf0bacb7fd3d58108e6c843e1ce6fcb
GAGO <gago1980@hotmail.es>
2025-11-19 20:12:16 +0100

    suma y resta

--- src/main/java/com/houarizegai/calculator/App.java
+++ src/main/java/com/houarizegai/calculator/App.java
@@ -6,5 +6,9 @@ public class App {
 
     public static void main(String[] args) {
         new CalculatorUI();
+        system.out.println("suma");
+        system.out.println("resta");
+        
+        
     }
 }
